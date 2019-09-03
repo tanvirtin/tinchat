1d7ef2a6599ce56aa2492209beb618c07a27f8e6
tanvirtin <tanvir.tinz@gmail.com>
2019-09-02 22:28:28 -0400

    Basic boilerplate configurations

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const port = process.env.SERVER_PORT || 8080;
+    await app.listen(port);
+    Logger.log(`Server is running on port: ${port}`, 'boostrap');
 }
 bootstrap();
